cc7d76690e16232c7cce69142823dc60295ca096
MudHutOnTheMoon <166847724+dogamongstmen@users.noreply.github.com>
2024-04-14 17:56:15 -0400

    Card collections endpoints work.

--- backend/application/endpoints/api/cardsets.py
+++ backend/application/endpoints/api/cardsets.py
@@ -5,7 +5,7 @@ from db.types.cardset import CardSet
 from server.handling.request import Request
 from server.handling.response import Response
 
-from pymongo.collection import Collection
+from pymongo.collection import Collection, InsertOneResult
 
 
 # Method: GET
@@ -41,5 +41,29 @@ def api_get_card_set_handler(
 def api_create_card_set_handler(
     state: ApplicationState, req: Request[None], res: Response
 ) -> None:
-    # req._buffer
-    res.json(json.dumps({"message": "Added card"}))
+    try:
+        body: CardSet = json.loads(req._buffer.read(int(req.headers["content-length"])))
+
+        if body["name"] == None:
+            res.status(400).json(
+                '{"error":"Request body must contain a `name` field."}'
+            )
+            return
+
+        card_set_collection: Collection[CardSet] = state.card_data_db.get_collection(
+            COLLECTION_CARD_SETS
+        )
+
+        insert_data: CardSet = {  # type: ignore
+            "name": body["name"],
+            "cover_img": None,
+            "card_ids": [],
+        }
+
+        result: InsertOneResult = card_set_collection.insert_one(insert_data)
+
+        print(result)
+
+        res.json(json.dumps({"set_id": str(result.inserted_id)}))
+    except:
+        res.status(400).json('{"error": "Malformed request data."}')
